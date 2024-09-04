fafd2aa59c452706ed02b8391bb042aa2c386b83
AsN00 <asn@odds.team>
2024-09-04 12:12:18 +0700

    add show view and create button in index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
   def index
     @users =User.all
   end
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
   @user = User.new
   end
@@ -9,11 +13,13 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to @user
+      # redirect_to @user
+      redirect_to root_path
     else
       render :new, status: :unprocessable_entity
     end
   end
+
 
 private
   def user_params
