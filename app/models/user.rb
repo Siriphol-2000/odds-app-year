e70f6045cebab84339c709db79d4a8714856a42d
AsN00 <asn@odds.team>
2024-09-10 09:04:41 +0700

    validate must be at least 18 year old

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
   validates :first_name, :last_name, :email, :phone, :subject,:email,:gender,:birthday, presence: true
+  validate :must_be_at_least_18_year_old
+
+private
+def must_be_at_least_18_year_old
+  if birthday.present?&&birthday>18.year.ago.to_date
+    errors.add(:birthday ,"You must be at least 18 year old")
+  end
+end
 end
